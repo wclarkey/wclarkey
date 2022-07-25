d675d1f1bc4a7522b1a120fdc37f9b2cafb68dfb
Will Clarke <clarke19w@gmail.com>
2022-07-25 13:44:47 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors:{
+                'backgroundColor':'#12141D',
+                'backgroundAccent':'#1E2029',
+                'backgroundAccentLighter':'#22242d',
+                'accent':'#DC3962',
+                'lightAccent':'#FF715B',
+                'primaryText':'#FFFFFF',
+                'secondaryText':'#6B8594'
+            }
+        }
+    }
+}
